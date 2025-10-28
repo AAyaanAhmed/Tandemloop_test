c887728995ab9aafcc7a56aa6857e0f18c19cd9e
AAyaanAhmed <153097603+AAyaanAhmed@users.noreply.github.com>
2025-10-28 18:33:44 +0530

    Add files via upload

---
+++ firstcode.py
@@ -0,0 +1,26 @@
+class calculator:
+    def __init__(self, a,b, op):
+        self.a = float(a)
+        self.b =float(b)
+        self.op = op
+
+    def calc(self):
+        if self.op == '+':
+            return self.a + self.b
+        elif self.op == '-':
+            return self.a - self.b
+        elif self.op == '*':
+            return self.a * self.b
+        elif self.op == '/':
+            return self.a / self.b
+        else:
+            return "Incorrect Input"
+
+
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+op = input("Enter operation(+, -,*,/): ")
+
+output = calculator(a, b, op)
+result = output.calc()
+print("Result:", result)
